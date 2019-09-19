fa9319030c80b76870f7154678c61ccfcbcc33ea
Alan <alanyao2002@gmail.com>
2019-09-19 09:40:18 -0700

    title

--- maze.cpp
+++ maze.cpp
@@ -2,15 +2,28 @@
 #include <iomanip>
 #include <cmath>
 #include <cstdlib>
+#include <string>
 
 using namespace std;
 
 int main()
 {
 	int counter = 0; //keeps track of deaths
-	cout << endl << "A Maze" << endl << endl;
+	cout << "    _        _    _      _      ____    ____" << endl;
+	cout << "   / \\      | \\  / |    / \\       /    |" << endl;
+	cout << "  /---\\     |  \\/  |   /---\\     /     |---" << endl;
+	cout << " /     \\    |      |  /     \\   /___   |____ " << endl << endl;
 	cout << "Press Enter to Begin...";
 	cin.ignore();
 	cout << endl;
 	return 0;
 }
+
+
+/*  
+    _        _    _      _      ____    ____
+   / \      | \  / |    / \       /    |
+  /---\     |  \/  |   /---\     /     |---
+ /     \    |      |  /     \   /___   |____  
+
+*/
