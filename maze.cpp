bfca4dc0c02489a6b87833905985af89b680a579
Alan <alanyao2002@gmail.com>
2019-09-24 12:16:16 -0700

    change tabs to spaces

--- maze.cpp
+++ maze.cpp
@@ -9,64 +9,62 @@ using namespace std;
 
 namespace player_board
 {
-	void generate_maze()
-	{
-		char maze[8][9];
-		for (int i = 0; i < 8; i++) 
-		{
-			for (int j = 0; j < 9; j++)
-			{
-				maze[i][j] = '0';
-			}
-		}
+    void generate_maze()
+    {
+        char maze[8][9];
+        for (int i = 0; i < 8; i++) 
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                maze[i][j] = '0';
+            }
+        }
 
-		for (int i = 0; i < 8; i++) 
-		{
-			for (int j = 0; j < 9; j++)
-			{
-				cout << maze[i][j] << " ";
-			}
-			cout << endl;
-		}
-	}
+        for (int i = 0; i < 8; i++) 
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                cout << maze[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
 }
 namespace dev_board
 {
-	void generate_maze()
-	{
-		char maze[9][9];
-		for (int i = 0; i < 9; i++) 
-		{
-			for (int j = 0; j < 9; j++)
-			{
-				maze[i][j] = '0';
-			}
-		}
-	}
+    void generate_maze()
+    {
+        char maze[9][9];
+        for (int i = 0; i < 9; i++) 
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                maze[i][j] = '0';
+            }
+        }
+    }
 }
 
 int main()
 {
-	int counter = 0; //keeps track of deaths
-	cout << "    _        _    _      _      ____    ____" << endl;
-	usleep(150000);
-	cout << "   / \\      | \\  / |    / \\       /    |" << endl;
-	usleep(150000);
-	cout << "  /---\\     |  \\/  |   /---\\     /     |---" << endl;
-	usleep(150000);
-	cout << " /     \\    |      |  /     \\   /___   |____ " << endl << endl;
-	usleep(150000);
-	cout << "Press Enter to Begin...";
-	cin.ignore();
-	usleep(100000);
-	cout << endl;
+    int counter = 0; //keeps track of deaths
+    cout << "    _        _    _      _      ____    ____" << endl;
+    usleep(150000);
+    cout << "   / \\      | \\  / |    / \\       /    |" << endl;
+    usleep(150000);
+    cout << "  /---\\     |  \\/  |   /---\\     /     |---" << endl;
+    usleep(150000);
+    cout << " /     \\    |      |  /     \\   /___   |____ " << endl << endl;
+    usleep(150000);
+    cout << "Press Enter to Begin...";
+    cin.ignore();
+    usleep(100000);
+    cout << endl;
 
-	player_board::generate_maze();
-	dev_board::generate_maze();
-	
-
-
-	return 0;
+    player_board::generate_maze();
+    dev_board::generate_maze();
+    
+    return 0;
 }
 
 
