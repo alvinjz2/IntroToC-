e2300aec8a42708f9d4ff3fd3672e83eb768bef5
alvinzhang295 <31424162+alvinzhang295@users.noreply.github.com>
2019-09-25 10:14:46 -0700

    Update maze.cpp

--- maze.cpp
+++ maze.cpp
@@ -40,6 +40,7 @@ namespace dev_board
 
 int main()
 {
+	int moves = 15;
     int counter = 0; //keeps track of deaths
     cout << "    _        _    _      _      ____    ____" << endl;
     usleep(150000);
@@ -54,11 +55,34 @@ int main()
     usleep(100000);
     cout << endl;
 
+    string command; 
     player_board::generate_maze();
     dev_board::generate_maze();
     
-    string command; 
     cin >> command; 
+    // do some 'tolower()' function on 'command' to make it lowercase 
+    while (moves > 0)
+    {
+    	if(command == "up")
+    	{
+    		moves--;
+    	}
+    	else if( command = "down")
+    	{
+    		moves--;
+    	}
+    	else if(command = "left")
+    	{
+    		moves--;
+    	}
+    	else if(commmand = "right")
+    	{
+    		moves--;
+    	}
+    	else{
+    		cout << "Type only up, down, left, or right" << endl;
+    	}
+    }
     
 
     return 0;
